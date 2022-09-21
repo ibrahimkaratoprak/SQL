2ead5e252c82b746aaeb46e20afe6099c0cdf680
ibrahim.karatoprak <113822764+ibrahimkaratoprak@users.noreply.github.com>
2022-09-21 21:58:47 +0300

    Create SQL-ÖDEV 1.sql
    SQL ÖDEV-1

---
+++ SQL-ÖDEV 1.sql
@@ -0,0 +1,5 @@
+--select title,description from film
+--select * from film where film.length> 60 and film.length < 75
+--select * from film   where film.rental_rate=0.99 and (film.replacement_cost=12.99 or film.replacement_cost =28.99 )
+--SELECT * from customer where customer.first_name='Mary'
+--select * from film where not film.length >50 and not  (film.rental_rate=2.99 or film.rental_rate=4.99)
