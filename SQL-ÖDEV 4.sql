cd1bf3956b82cea0202971728a136534c2f1aa1e
ibrahim.karatoprak <113822764+ibrahimkaratoprak@users.noreply.github.com>
2022-09-22 15:32:42 +0300

    Create SQL-ÖDEV 4.sql

---
+++ SQL-ÖDEV 4.sql
@@ -0,0 +1,5 @@
+--SELECT DISTINCT replacement_cost from film
+--SELECT count(DISTINCT replacement_cost) from film
+--select count(title) from film where title like 'T%' and rating='G'
+--select count(*) from country where country like '_____'
+--select count(*) from city where city ilike '%r'
