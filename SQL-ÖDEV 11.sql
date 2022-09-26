36bab84500e7ec57e5ccd9c1697101e00d4ff40d
ibrahim.karatoprak <113822764+ibrahimkaratoprak@users.noreply.github.com>
2022-09-26 18:26:36 +0300

    Create SQL-ÖDEV 11.sql

---
+++ SQL-ÖDEV 11.sql
@@ -0,0 +1,6 @@
+--select first_name from actor union select first_name from customer
+--select first_name from actor intersect select first_name from customer
+--select first_name from actor except select first_name from customer
+(select first_name from actor union all select first_name from customer) intersect all 
+(select first_name from actor intersect  select first_name from customer) intersect all 
+(select first_name from actor except all select first_name from customer) 
