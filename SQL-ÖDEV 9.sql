414284a2df38c65de40754ece121687530ba3745
ibrahim.karatoprak <113822764+ibrahimkaratoprak@users.noreply.github.com>
2022-09-26 11:55:21 +0300

    Create SQL-ÖDEV 9.sql

---
+++ SQL-ÖDEV 9.sql
@@ -0,0 +1,3 @@
+--select country,city from country inner join city on country.country_id=city.country_id order by 1
+--select payment_id,first_name,last_name from customer join payment  on payment.customer_id=customer.customer_id
+--select rental_id,first_name,last_name from rental join customer on customer.customer_id=rental.customer_id
